a17f66e2998baf4d7da94b8aef114cabd84387a7
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2022-08-04 18:36:25 +0300

    added 6th lesson task solving

--- lesson-06/index.js
+++ lesson-06/index.js
@@ -1,3 +1,46 @@
+// const user = {
+//   email: 'mango@mail.com',
+//   age: 20,
+// };
+
+// const email = 'poly@mail.com';
+
+// console.log(user.age); // 20
+// console.log(user.name); // undefined
+
+// const { username } = user; // === const username = user.username
+// const { email: userEmail } = user; // === const userEmail = user.email
+// console.log(email);
+
+// console.log([1, 3, 5] === [1, 3, 5]);
+
+// const a = [1, 3, 5];
+// // const b = a;
+// const b = [...a];
+
+// console.log(b === a);
+
+// function getUser(name = 'Sasha') {
+//   console.log(name);
+// }
+
+// getUser();
+
+// const first = { A: 1, B: 2 };
+// const second = { A: 3, C: 4 };
+// const third = {
+//   A: 5,
+//   ...first, // { A: 1, B: 2 }
+//   ...second, // { B: 2, A: 3, C: 4 }
+//   C: 6, // { B: 2, A: 3, C: 6 }
+// };
+
+// function qwe(a, b, c, asf, fas, gas, ...args) {
+//   console.log(args);
+// }
+
+// qwe({});
+
 // # Модуль 3 Занятие 6. Деструктуризация и rest/spread
 
 // ## Example 1 - Деструктуризация
@@ -5,7 +48,9 @@
 // Перепиши функцию так, чтобы она принимала один объект параметров, вместо набора
 // независимых аргументов.
 
-// function calcBMI(weight, height) {
+// function calcBMI({ weight, height }) {
+//   // const { weight, height } = params;
+
 //   const numericWeight = Number(weight.replace(',', '.'));
 //   const numericHeight = Number(height.replace(',', '.'));
 //   return Number((numericWeight / numericHeight ** 2).toFixed(1));
@@ -16,12 +61,17 @@
 // // console.log(calcBMI('68,3', '1.65'));
 // // console.log(calcBMI('118,3', '1.95'));
 
+// Стало
+// console.log(calcBMI({ weight: '88,3', height: '1.75' }));
+// console.log(calcBMI({ weight: '68,3', height: '1.65' }));
+// console.log(calcBMI({ weight: '118,3', height: '1.95' }));
+
 // ## Example 2 - Деструктуризация
 
 // Перепиши функцию так, чтобы она принимала один объект параметров, вместо набора
 // независимых аргументов.
 
-// function printContactsInfo(names, phones) {
+// function printContactsInfo({ names, phones }) {
 //   const nameList = names.split(',');
 //   const phoneList = phones.split(',');
 //   for (let i = 0; i < nameList.length; i += 1) {
@@ -35,23 +85,55 @@
 // //   '89001234567,89001112233,890055566377,890055566300',
 // // );
 
+// const users = {
+//   names: 'Jacob,William,Solomon,Artemis',
+//   phones: '89001234567,89001112233,890055566377,890055566300',
+// };
+
+// // Стало
+// printContactsInfo(users);
+
 // ## Example 3 - Глубокая деструктуризация
 
 // Перепиши функцию так, чтобы она принимала один объект параметров, вместо набора
 // независимых аргументов.
 
-// function getBotReport(companyName, repairBots, defenceBots) {
+// function getBotReport({ companyName, bots: { repairBots, defenceBots } }) {
 //   return `${companyName} has ${repairBots + defenceBots} bots in stock`;
 // }
 
 // // Было
 // // console.log(getBotReport('Cyberdyne Systems', 150, 50));
 
+// // Стало
+// console.log(
+//   getBotReport({
+//     companyName: 'Cyberdyne Systems',
+//     bots: {
+//       repairBots: 150,
+//       defenceBots: 50,
+//     },
+//   }),
+// );
+
 // ## Example 4 - Деструктуризация
 
 // Перепиши функцию так, чтобы она принимала объект параметров со свойствами
 // `companyName` и `stock` и выводила репорт о количестве товаров на складе любой
 // компании.
+
+// function getStockReport({ companyName, stock }) {
+//   // console.log(stock); // {........}
+//   let total = 0;
+//   const stockValues = Object.values(stock);
+
+//   for (const value of stockValues) {
+//     total += value;
+//   }
+
+//   // console.log(total);
+//   return `${companyName} has ${total} items in stock`;
+// }
 
 // console.log(
 //   getStockReport({
@@ -76,9 +158,30 @@
 
 // ## Example 5 - Операция spread
 
-// Дополни функцию `createContact(partialContact)` так, чтобы она возвращала новый
+// Напиши функцию `createContact(partialContact)` так, чтобы она возвращала новый
 // объект контакта с добавленными свойствами `id` и `createdAt`, а также `list` со
 // значением "default" если в `partialContact` нет такого свойства.
+
+// function createContact(partialContact) {
+//   // console.log(partialContact);
+
+//   // const newContact = partialContact;
+//   // newContact.id = generateId();
+//   // newContact.createdAt = Date.now();
+//   const newContact = {
+//     list: 'default',
+//     ...partialContact,
+//     id: generateId(),
+//     createdAt: Date.now(),
+//   };
+
+//   // if (partialContact.list) {
+//   // } else {
+//   //   newContact.list = 'default';
+//   // }
+
+//   return newContact;
+// }
 
 // console.log(
 //   createContact({
@@ -103,18 +206,41 @@
 // Напиши функцию `transformUsername(user)` так, чтобы она возвращала новый обьект
 // со свойством `fullName`, вместо `firstName` и `lastName`.
 
+// function transformUsername({ id, email, friendCount, ...names }) {
+//   // console.log(names);
+
+//   const newUser = {
+//     id,
+//     email,
+//     friendCount,
+//     fullname: `${names.firstName} ${names.lastName}`,
+//   };
+
+//   return newUser;
+// } // НЕ ОЧЕНЬ ПРАВИЛЬНО!
+
+// function transformUsername({ firstName, lastName, ...user }) {
+//   const newUser = {
+//     ...user,
+//     fullname: `${firstName} ${lastName}`,
+//   };
+
+//   return newUser;
+// }
+
 // console.log(
-//   transformId({
+//   transformUsername({
 //     id: 1,
 //     firstName: 'Jacob',
 //     lastName: 'Mercer',
 //     email: 'j.mercer@mail.com',
 //     friendCount: 40,
+//     list: 'friend',
 //   }),
 // );
 
 // console.log(
-//   transformId({
+//   transformUsername({
 //     id: 2,
 //     firstName: 'Adrian',
 //     lastName: 'Cross',
